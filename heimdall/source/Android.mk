e1c39f75149563cc627148e6fa4db5f524c6b146
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-05 20:35:46 +0300

    Port heimdall to Android
    Change-Id: Ie848da05f37fee4a32c00ea956b983beaf64117a

--- heimdall/source/Android.mk
+++ heimdall/source/Android.mk
@@ -1,8 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
+common_files := \
   Arguments.cpp \
   ClosePcScreenAction.cpp \
   DownloadPitAction.cpp \
@@ -17,7 +15,11 @@ LOCAL_SRC_FILES := \
   Utility.cpp \
   main.cpp \
   ../../libpit/Source/libpit.cpp
-  
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(common_files)
+ 
 LOCAL_C_INCLUDES := \
   external/heimdall/libpit/Source \
   external/libusbx/libusb
@@ -34,5 +36,37 @@ endif
 
 LOCAL_MODULE := heimdall
 include $(BUILD_HOST_EXECUTABLE)
+
+##### Device binary
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(common_files)
+
+LOCAL_C_INCLUDES := \
+  external/heimdall/libpit/Source \
+  external/libusbx/libusb \
+  external/stlport/stlport
+
+LOCAL_C_INCLUDES += bionic
+
+#  /usr/include/c++/4.8
+##  prebuilts/ndk/9/sources/cxx-stl/gnu-libstdc++/4.8/libs/armeabi-v7a/include \
+#  prebuilts/ndk/9/sources/cxx-stl/gnu-libstdc++/4.8/include
+
+LOCAL_STATIC_LIBRARIES := libc libusbx libstlport_static
+#libc libstdc++ libc++
+
+#ifeq ($(HOST_OS),linux)
+#LOCAL_LDFLAGS := -lpthread -lrt
+#endif
+
+LOCAL_CFLAGS := -Wno-non-virtual-dtor
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_MODULE := heimdall
+#LOCAL_CLANG := true
+include $(BUILD_EXECUTABLE)
 
 $(call dist-for-goals,dist_files sdk,$(LOCAL_BUILT_MODULE))
